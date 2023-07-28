e6ca43a19b033f068ddfa589d51d1cecd308f057
qvixnh <nguyenvinh22012002@gmail.com>
2023-07-28 09:07:44 +0700

    set up users Dbset before creating a migration

---
+++ ClubMembershipApplication/Data/ClubMembershipDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ClubMembershipApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.Data
+{
+    public class ClubMembershipDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //connection string: establish the path to our sqlitle db file 
+            optionsBuilder.UseSqlite($"Data Source={AppDomain.CurrentDomain.BaseDirectory}ClubMembershipDb.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+        //create a table database structure for relevant model classes
+        public DbSet<User> Users { get; set; }
+        //-use DbSet generic type to (contain a reference to the user table )
+        //EF uses "Users" to add users to "database tables" and retrieves data  from "database table"
+        //---create a migration
+    }
+}
--- ClubMembershipApplication/Models/User.cs
+++ ClubMembershipApplication/Models/User.cs
@@ -13,7 +13,6 @@ namespace ClubMembershipApplication.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
         public string Password{ get; set; }
         public DateTime DateOfBirth { get; set; }
         public string PhoneNumber { get; set; }
