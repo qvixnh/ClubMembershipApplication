40735da515582d3b23954755746d35f93f265b74
qvixnh <nguyenvinh22012002@gmail.com>
2023-07-30 14:18:03 +0700

    Implementing the ILogin interface and creating factory static class

---
+++ ClubMembershipApplication/Factory.cs
@@ -0,0 +1,29 @@
+﻿using ClubMembershipApplication.Data;
+using ClubMembershipApplication.FieldValidators;
+using ClubMembershipApplication.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication
+{
+    public static class Factory
+    {
+        
+        public static IView GeMainViewObject()
+        {
+            //used: Identicate the user
+            ILogin login = new LoginUser();
+            IRegister register = new RegisterUser();
+            IFieldValidator userRegistrationValidator = new UserRegistrationValidator(register);
+            userRegistrationValidator.InitialiseValidatorDelegates();
+            IView registerView = new UserRegistrationView(register, userRegistrationValidator);
+            IView loginView = new UserLoginView(login);
+            IView mainView = new MainView(registerView, loginView);
+
+            return mainView;
+        }
+    }
+}
